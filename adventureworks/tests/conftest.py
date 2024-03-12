acac727c5388a823a89f5ebcbbad16270ed99821
JosephMachado <joseph.machado.professional@gmail.com>
2024-03-12 11:16:46 -0400

    commit

--- adventureworks/tests/conftest.py
+++ adventureworks/tests/conftest.py
@@ -3,21 +3,22 @@ import pytest
 from delta import configure_spark_with_delta_pip
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def spark():
     my_packages = [
         "io.delta:delta-core_2.12:2.3.0",
         "org.apache.hadoop:hadoop-aws:3.3.2",
     ]
 
+    import os
+
+    print(os.environ.get("PYTHONPATH", ""))
     builder = (
         pyspark.sql.SparkSession.builder.appName("adventureworks_tests")
         .config("spark.executor.cores", "1")
         .config("spark.executor.instances", "1")
         .config("spark.sql.shuffle.partitions", "1")
-        .config(
-            "spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension"
-        )
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config(
             "spark.sql.catalog.spark_catalog",
             "org.apache.spark.sql.delta.catalog.DeltaCatalog",
